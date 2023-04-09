a91dbc7d2cfb74cc9f48678d618a63c6ca0c429a
Klosowsky <mateuszklos2000@interia.pl>
2023-04-09 23:41:46 +0200

    Progress with addtraining view and started creating dynamic form with new training

--- public/views/addtraining.php
+++ public/views/addtraining.php
@@ -10,6 +10,21 @@
 
     </script>
     <script src="https://kit.fontawesome.com/ab1fdc6776.js" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="src/js/jquery.js"></script>
+    <script type="text/javascript">
+        function add_row()
+        {
+            $rowno=1;
+
+            $("#add-ex-1-1").after('<div class="add-excercise-details" id="add-ex-1-2"> <input type="text" placeholder="exercise nie2"> </div>');
+        }
+        function delete_row(rowno)
+        {
+            $('#'+rowno).remove();
+        }
+    </script>
+
 </head>
 <body>
 
@@ -26,6 +41,42 @@
             </div>
         </div>
 
+        <div class="add-training-desc-box">
+            <div class="add-training-desc-header">
+                <p>Description</p>
+            </div>
+            <div class="add-training-desc-content">
+                <textarea maxlength="50" class="new-training-desc" placeholder="Your description..."></textarea>
+            </div>
+        </div>
+
+        <div class="add-training-buttons-box">
+            <div class="upload-new-training">
+                Add training
+            </div>
+            <div class="delete-new-training">
+                Add training
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <div class="training-days-container">
+        <form action="/add-training" method="post">
+            <div class="training-day-box" id="add-day-1">
+                <div class="training-box-day-number">
+                    <p>Day 1</p>
+                </div>
+                <div class="training-box-exercise-list">
+                    <div class="add-excercise-details" id="add-ex-1-1">
+                        <input type="text" placeholder="exercise nie">
+                    </div>
+
+                </div>
+            </div>
+        </form>
     </div>
 
 
